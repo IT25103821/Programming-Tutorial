b532eaccc92b4c836663279ddacddc32380296ef
IT25103821 <IT25103821@my.sliit.lk>
2026-02-08 23:20:38 +0530

    IT25103821Q4B.java to Q4

---
+++ IT25103821Q4B.java
@@ -0,0 +1,14 @@
+public class IT25103821Q4B{
+    public static void main(String[] args){
+
+        for (int i = 1; i <= 5; i++){
+            for (int s = 5; s > i; s--){
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*   ");
+            }
+            System.out.println();
+        }
+    }
+}
