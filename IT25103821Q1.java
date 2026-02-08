b4754a87a84ed359b99a751fef0c3d00b955bce9
IT25103821 <IT25103821@my.sliit.lk>
2026-02-08 23:17:52 +0530

    IT25103821Q1.java to Q1

---
+++ IT25103821Q1.java
@@ -0,0 +1,13 @@
+public class IT25103821Q1{
+	public static void main(String[] args){
+		
+		int miles = 26;
+		int yards = 385;
+		double kilometers;
+		
+		//convert miles and yards to kilometers
+		kilometers = (miles + (yards / 1760.0))*1.609;
+		
+		System.out.println("Distance in kilometers: " + kilometers);
+	}
+}
